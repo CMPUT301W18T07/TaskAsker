f07746b9853ad6888a113bedd9b94bb1e2987b6e
Tyler Strembitsky <tylerstrembitsky@gmail.com>
2018-03-09 14:29:31 -0700

    Added base activities and basic login UI

---
+++ app/src/main/java/com/cmput301w18t07/taskasker/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.cmput301w18t07.taskasker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}

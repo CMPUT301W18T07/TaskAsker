1b0a2a121b655bac07a14dac04304d913128e01d
christopher-M-Wood <cmwood@ualberta.ca>
2018-03-13 14:31:24 -0600

    Basic implementation of adding a new user (doesn't actually save the user)

--- app/src/main/java/com/cmput301w18t07/taskasker/NewAccountActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/NewAccountActivity.java
@@ -2,12 +2,51 @@ package com.cmput301w18t07.taskasker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewAccountActivity extends AppCompatActivity {
+    private EditText firstName;
+    private EditText lastName;
+    private EditText username;
+    private EditText email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_account);
+
+        firstName = (EditText) findViewById(R.id.firstNameEditText);
+        lastName = (EditText) findViewById(R.id.lastNameEditText);
+        username = (EditText) findViewById(R.id.usernameEditText);
+        email = (EditText) findViewById(R.id.emailEditText);
+
+        Button signupButton = (Button) findViewById(R.id.signupButton);
+        Button cancelButton = (Button) findViewById(R.id.cancelButton);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setResult(RESULT_OK);
+                String firstNameText = firstName.getText().toString();
+                String lastNameText = lastName.getText().toString();
+                String usernameText = username.getText().toString();
+                String emailText = email.getText().toString();
+                String phoneNumber = "000-000-0000";
+                try{
+                    User user = new User(usernameText,emailText,phoneNumber,firstNameText,lastNameText);
+                }
+                catch(Exception e){}
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Todo: Exit back to login
+                setResult(RESULT_OK);
+            }
+        });
     }
 }

4f7f702464a7af13cd9af8a0d1be1659559ecc0a
Brendan Bartok <bbartok@ualberta.ca>
2018-03-15 20:06:44 -0600

    made Buttons do something

--- app/src/main/java/com/cmput301w18t07/taskasker/ProfileActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/ProfileActivity.java
@@ -1,13 +1,40 @@
 package com.cmput301w18t07.taskasker;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    private ProfileActivity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+
+        final Button cancelButton = findViewById(R.id.cancelButton);
+        final Button confirmButton = findViewById(R.id.confirmButton);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO add check for changed profile as well as commiting it to database
+                Intent intent = new Intent(activity, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }

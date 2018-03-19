baeb552d87a51fb74da96773e514c8fbb0d6c7bf
Thomas Mackay <TMackay12@gmail.com>
2018-03-19 15:32:11 -0600

    Search Activity Basics

--- app/src/main/java/com/cmput301w18t07/taskasker/SearchActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/SearchActivity.java
@@ -1,8 +1,32 @@
 package com.cmput301w18t07.taskasker;
 
-/**
- * Created by brend on 2018-03-19.
- */
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
-public class SearchActivity {
+import java.util.ArrayList;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private String url = "http://cmput301.softwareprocess.es:8080/cmput301w18t07";
+    private SearchController controller = new SearchController(url);
+    private ArrayList<Task> openTaskList;
+    private TaskListAdapter taskAdapter;
+    private ListView taskListView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        //taskListView = findViewById(R.id.???);
+
+        //openTaskList = controller.getOpenTasks();
+
+        taskAdapter = new TaskListAdapter(getApplicationContext(), openTaskList);
+
+        taskListView.setAdapter(taskAdapter);
+
+    }
+
 }

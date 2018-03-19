9015b7c51cdf776d743a0230c0e2df09aced7eef
christopher-M-Wood <cmwood@ualberta.ca>
2018-03-19 13:00:28 -0600

    More Bid Testing

--- app/src/test/java/com/cmput301w18t07/taskasker/BidTest.java
+++ app/src/test/java/com/cmput301w18t07/taskasker/BidTest.java
@@ -19,12 +19,14 @@ public class BidTest {
     User firstUser;
     User secondUser;
     Task firstTask;
+    Task secondTask;
 
     public BidTest(){
         try{
             firstUser = new User("Test", "test@fake.com", "000-000-0000", "Test", "Faker");
             secondUser = new User("Next", "next@fake.com", "000-000-0001", "Next", "Faker");
             firstTask = new Task("Primary", "Basic task", firstUser);
+            secondTask = new Task("Secondary", "Another basic task", secondUser);
         } catch(Exception e){
             e.printStackTrace();
             assertTrue(false);
@@ -35,6 +37,33 @@ public class BidTest {
     public void createTest(){
         Bid bid = new Bid(secondUser, 100.0,firstTask);
         assertNotNull(bid);
+    }
+
+    @Test
+    public void getUserTest(){
+        Bid bid = new Bid(secondUser, 100.0, firstTask);
+        assertEquals(secondUser, bid.getBidder());
+
+        bid = new Bid(firstUser, 100.0, firstTask);
+        assertEquals(firstUser, bid.getBidder());
+    }
+
+    @Test
+    public void getBidTest(){
+        Bid bid = new Bid(secondUser, 100.0, firstTask);
+        assertEquals(100.0, bid.getBid());
+
+        bid = new Bid(secondUser, 55.227, firstTask);
+        assertEquals(55.227, bid.getBid());
+    }
+
+    @Test
+    public void getTaskTest(){
+        Bid bid = new Bid(secondUser, 100.0, firstTask);
+        assertEquals(firstTask, bid.getTask());
+
+        bid = new Bid(secondUser, 100.0, secondTask);
+        assertEquals(secondTask, bid.getTask());
     }
 
 }

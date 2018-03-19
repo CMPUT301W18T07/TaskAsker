352e73d8db22295b613b91f55eb83cca9daa5c3d
Thomas Mackay <TMackay12@gmail.com>
2018-03-19 15:34:37 -0600

    Minor Search changes

--- app/src/main/java/com/cmput301w18t07/taskasker/SearchActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/SearchActivity.java
@@ -23,6 +23,12 @@ public class SearchActivity extends AppCompatActivity {
 
         //openTaskList = controller.getOpenTasks();
 
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         taskAdapter = new TaskListAdapter(getApplicationContext(), openTaskList);
 
         taskListView.setAdapter(taskAdapter);

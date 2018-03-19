e1e4a6f22707be86326539d34fdbc632e2cd234a
Brendan Bartok <bbartok@ualberta.ca>
2018-03-19 15:39:35 -0600

    added activity_search compat

--- app/src/main/java/com/cmput301w18t07/taskasker/SearchActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/SearchActivity.java
@@ -1,8 +1,25 @@
+/*
+ * Copyright (c) 2018 Brendan Bartok, Christopher Wood, Dylan Alcock, Lucas Gauk, Thomas Mackay,
+ * Tyler Strembitsky, CMPUT301, University of Alberta - All Rights Reserved. You may use,
+ * distribute, or modify this code under terms and conditions of the Code of Student Behaviour
+ *  at University of Alberta. You can find a copy of the license on this project.
+ */
+
 package com.cmput301w18t07.taskasker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 /**
  * Created by brend on 2018-03-19.
  */
 
-public class SearchActivity {
+public class SearchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
 }

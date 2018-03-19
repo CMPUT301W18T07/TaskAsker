8bc64d109f01c8234acd8304ea5f5f738008d86d
Thomas Mackay <TMackay12@gmail.com>
2018-03-19 15:42:02 -0600

    Fixing Git Issues

--- app/src/main/java/com/cmput301w18t07/taskasker/SearchActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/SearchActivity.java
@@ -7,19 +7,39 @@
 
 package com.cmput301w18t07.taskasker;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
 
-/**
- * Created by brend on 2018-03-19.
- */
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private String url = "http://cmput301.softwareprocess.es:8080/cmput301w18t07";
+    private SearchController controller = new SearchController(url);
+    private ArrayList<Task> openTaskList;
+    private TaskListAdapter taskAdapter;
+    private ListView taskListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+
+        //taskListView = findViewById(R.id.???);
+
+        //openTaskList = controller.getOpenTasks();
+
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        taskAdapter = new TaskListAdapter(getApplicationContext(), openTaskList);
+
+        taskListView.setAdapter(taskAdapter);
+
     }
+
 }

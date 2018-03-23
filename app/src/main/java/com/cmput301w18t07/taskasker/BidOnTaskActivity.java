2e655e41e05e4410899ce498eb8a141bb862c438
christopher-M-Wood <cmwood@ualberta.ca>
2018-03-23 10:29:39 -0600

    Small tweak

--- app/src/main/java/com/cmput301w18t07/taskasker/BidOnTaskActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/BidOnTaskActivity.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -50,6 +51,8 @@ public class BidOnTaskActivity extends AppCompatActivity {
         final TextView title = findViewById(R.id.title);
         final TextView lowestBid = findViewById(R.id.lowestbid);
         final TextView lowestBidText = findViewById(R.id.lowestBidText);
+
+        final EditText bidEditText = findViewById(R.id.bidEditText);
 
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override

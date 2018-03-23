f692bc1a62a42e31a9b67b8ed1b8252332891ed1
christopher-M-Wood <cmwood@ualberta.ca>
2018-03-23 09:42:39 -0600

    Bidding activity

---
+++ app/src/main/java/com/cmput301w18t07/taskasker/BidOnTaskActivity.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2018 Brendan Bartok, Christopher Wood, Dylan Alcock, Lucas Gauk, Thomas Mackay,
+ * Tyler Strembitsky, CMPUT301, University of Alberta - All Rights Reserved. You may use,
+ * distribute, or modify this code under terms and conditions of the Code of Student Behaviour
+ *  at University of Alberta. You can find a copy of the license on this project.
+ */
+
+package com.cmput301w18t07.taskasker;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Purpose:
+ * Activity that allows placing of a bid on a task
+ *
+ * Design Rationale:
+ * Having a way to add a bid in a useful way
+ *
+ * @author Chris
+ * @version 1.5
+ * @see Bid
+ * @see Task
+ */
+
+public class BidOnTaskActivity extends AppCompatActivity {
+    private String url = "http://cmput301.softwareprocess.es:8080/cmput301w18t07";
+    private SearchController controller = new SearchController(url);
+    private BidOnTaskActivity activity = this;
+
+    /**
+     * Purpose:
+     * Sets up the view when the add a bid activity is started
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bid_on_task);
+        final int taskID = getIntent().getIntExtra("task ID", 0);
+        Task task = controller.getTaskById(taskID);
+
+        final Button backButton = findViewById(R.id.backButton);
+        final Button submitButton = findViewById(R.id.submitButton);
+
+        final TextView title = findViewById(R.id.title);
+        final TextView lowestBid = findViewById(R.id.lowestbid);
+        final TextView lowestBidText = findViewById(R.id.lowestBidText);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //@TODO: Add the new bid to the server
+            }
+        });
+    }
+}

d83e769b530a206cdc04a1ab8c07ee6da30c0a16
Thomas Mackay <TMackay12@gmail.com>
2018-04-04 17:23:18 -0600

    AcceptBidActivity Basic Layout

--- app/src/main/java/com/cmput301w18t07/taskasker/AcceptBidActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/AcceptBidActivity.java
@@ -7,14 +7,53 @@
 
 package com.cmput301w18t07.taskasker;
 
+import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class AcceptBidActivity extends AppCompatActivity {
+
+    private ListView bidListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accept_bid);
+
+        bidListView = findViewById(R.id.bidListView);
+
+        bidListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(AcceptBidActivity.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.accept_bid_dialogbox, null);
+                Button acceptButton = dialogView.findViewById(R.id.acceptButton);
+                Button deleteButton = dialogView.findViewById(R.id.deleteButton);
+                acceptButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //TODO: Functionality for accepting Bids
+                        //MAKE SURE CHANGE STATUS FIELD
+                    }
+                });
+                deleteButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //TODO: Functionality for deleting Bids
+                        //MAKE SURE TO CHECK TO SEE IF THERE ARE NO BIDS, IF SO CHANGE THE STATUS
+                    }
+                });
+                dialogBuilder.setView(dialogView);
+                AlertDialog dialogBox = dialogBuilder.create();
+                dialogBox.show();
+            }
+        });
+
     }
 }

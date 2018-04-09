7133bb266086aa7742a929569379e6680d60754a
Brendan Bartok <bbartok@ualberta.ca>
2018-04-08 18:58:02 -0600

    added location getting fucntion
    may drop you in the middle of the ocean

--- app/src/main/java/com/cmput301w18t07/taskasker/MapActivity.java
+++ app/src/main/java/com/cmput301w18t07/taskasker/MapActivity.java
@@ -8,6 +8,9 @@
 package com.cmput301w18t07.taskasker;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +21,8 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 /**
  * Purpose:
@@ -47,9 +52,31 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     public void onMapReady(GoogleMap googleMap) {
         gMap = googleMap;
 
+        double[] location = getGPS();
         // Add a marker in Sydney, Australia, and move the camera.
-        LatLng sydney = new LatLng(-34, 151);
+        LatLng sydney = new LatLng(location[0], location[1]);
         gMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         gMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+
+    private double[] getGPS(){
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        List<String> providers = locationManager.getProviders(true);
+
+        Location l = null;
+
+        for(int i=providers.size()-1;i>=0;i--){
+            l = locationManager.getLastKnownLocation(providers.get(i));
+            if(l!=null){
+                break;
+            }
+        }
+        double[] gps = new double[2];
+        if(l != null){
+            gps[0] = l.getLatitude();
+            gps[1] = l.getLongitude();
+        }
+
+        return gps;
     }
 }
